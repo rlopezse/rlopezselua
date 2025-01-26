35f5aabbbfd0162fba6e9510029a802b39e6e530
rlopezse <rza.lopez.s@gmail.com>
2025-01-26 11:41:33 -0300

    added: redo remap

--- lua/rlopezse/remap.lua
+++ lua/rlopezse/remap.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("n", "<leader>u", "<C-R>")
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
