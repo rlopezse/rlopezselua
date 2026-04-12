a78c609092d70efe6d0d8862bd487da1e1c9a22a
rlopezse <rza.lopez.s@gmail.com>
2026-04-12 00:03:40 -0400

    updated: config

--- lua/rlopezse/remap.lua
+++ lua/rlopezse/remap.lua
@@ -5,7 +5,16 @@ vim.keymap.set("n", "<leader>u", "<C-R>")
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gIc<Left><Left><Left><Left>]])
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+vim.keymap.set('n', '<leader>cc', '<cmd>ClaudeCode<CR>', {
+  desc = 'Claude Code (buffer)'
+})
+
+vim.keymap.set('v', '<leader>cc', '<cmd>ClaudeCode<CR>', {
+  desc = 'Claude Code (selection)'
+})
 
