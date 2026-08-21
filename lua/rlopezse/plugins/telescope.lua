234d6d20f2d93f04a78cf543c96093992ee086cd
rlopezse <rza.lopez.s@gmail.com>
2026-08-21 12:19:02 -0400

    ajustes shortcuts

--- lua/rlopezse/plugins/telescope.lua
+++ lua/rlopezse/plugins/telescope.lua
@@ -1,54 +1,18 @@
 return {
     'nvim-telescope/telescope.nvim',
     dependencies = {
-        'nvim-lua/plenary.nvim',
+        'nvim-lua/plenary.nvim'
     },
-
     config = function()
-        local telescope = require('telescope')
+        require('telescope').setup({})
+
         local builtin = require('telescope.builtin')
 
-        telescope.setup({
-            defaults = {
-                file_ignore_patterns = {
-                    "node_modules",
-                    "%.git/",
-                    "dist/",
-                    "build/",
-                    "coverage/",
-                },
-            },
-
-            pickers = {
-                find_files = {
-                    find_command = {
-                        "fd",
-                        "--type", "f",
-                        "--hidden",
-                        "--exclude", ".git",
-                        "--exclude", "node_modules",
-                        "--exclude", "dist",
-                        "--exclude", "build",
-                        "--exclude", "coverage",
-                    },
-                },
-            },
-        })
-
-        vim.keymap.set('n', '<leader>sf', builtin.find_files, {
-            desc = 'Find Files'
-        })
-
-        vim.keymap.set('n', '<leader>sg', builtin.live_grep, {
-            desc = 'Live Grep'
-        })
-
-        vim.keymap.set('n', '<leader>ps', function()
+        vim.keymap.set('n', '<leader>s', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>p', function()
             builtin.grep_string({
                 search = vim.fn.input("Grep > ")
             })
-        end, {
-            desc = 'Grep String'
-        })
-    end,
+        end)
+    end
 }
